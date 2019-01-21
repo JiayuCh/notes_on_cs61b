82ba54d31719995aebf1543a3b187d6dfe871b76
chenjiayu <jiayuchen1128@gmail.com>
2019-01-21 22:56:08 +0800

    organizes staff

---
+++ 3_References, Recursion and Lists/IntList.java
@@ -0,0 +1,37 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+    public int get(int i){
+        IntList p = this;
+        while (i > 0){
+            p = p.rest;
+            i--;
+        }
+        return p.first;
+    }
+}
